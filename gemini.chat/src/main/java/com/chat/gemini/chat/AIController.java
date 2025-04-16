ef9c31de5bc5573d6785218658bea1406f1de4ad
AmreshRajvinderSingh <144646953+AmreshRajvinderSingh@users.noreply.github.com>
2025-04-16 19:59:54 -0400

    added springboot application

---
+++ gemini.chat/src/main/java/com/chat/gemini/chat/AIController.java
@@ -0,0 +1,24 @@
+package com.chat.gemini.chat;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/qna")
+public class AIController {
+    private final QnAService qnAService;
+
+    @PostMapping("/ask")
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String, String> payload) {
+        String question = payload.get("question");
+        String answer = qnAService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
---
+++ gemini.chat/src/main/java/com/chat/gemini/chat/QnAService.java
@@ -0,0 +1,51 @@
+package com.chat.gemini.chat;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+public class QnAService {
+// access to api key and url[ Gemini]
+@Value("${gemini.api.url}")
+private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+    private final WebClient webClient;
+
+
+    public QnAService(WebClient.Builder webClient) {
+        this.webClient = WebClient.builder().build();
+    }
+
+    public String getAnswer(String question) {
+        // construct the request payload
+        Map<String, Object> requestBody = Map.of(
+                "contents", new Object[] {
+                        Map.of(
+                                "parts", new Object[] {
+                                        Map.of("text", question)
+                                }
+                        )
+                }
+        );
+        // {"content":[{"parts":[{"text":"question"}]}]}
+
+
+        // make api call
+        String resposne;
+        resposne=webClient.post()
+                .uri(geminiApiUrl+geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        // return response
+        return resposne;
+    }
+}
